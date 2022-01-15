9663f03f6cd15ff8cc8ec61d39781913ca23997c
Anelya <ssmanelya18@gmail.com>
2022-01-15 10:31:18 -0500

    created divide method

--- src/gitPractice/Calculator.java
+++ src/gitPractice/Calculator.java
@@ -15,4 +15,8 @@ public class Calculator {
         int result= num1-num2;
         return result;
     }
+    public static int divide(int num1, int num2){
+        int result= num1/num2;
+        return result;
+    }
 }
